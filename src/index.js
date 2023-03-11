7919f2b6f2ea48edee47b468433038ef767b6065
lza_menace <lza_menace@protonmail.com>
2023-03-10 23:50:28 -0800

    get basic app skeleton in place

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './css/normalize.css';
+import './css/skeleton.css';
+import './css/main.css';
+import Chevron from './chevron.png';
+
+const startToken = 1;
+const endToken = 5555;
+const url = "https://files.lzahq.tech/schizoposters/images/";
+
+function getRandomToken() {
+  return Math.floor(Math.random() * (endToken - startToken) + startToken)
+}
+
+function App() {
+  const [token, setToken] = useState(0);
+  if (token === 0) {
+    setToken(getRandomToken());
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <span className="schizoImage">
+          <h2>SCHIZOPOSTER #{token}</h2>
+          <a href={url + "schizoposter_" + token + ".png"} target="_blank" rel="noopener noreferrer">
+            <img src={url + "schizoposter_" + token + ".resized.png"} alt={"SCHIZOPOSTER " + token} />
+          </a>
+        </span>
+      </div>
+      <div className="row">
+        {token < endToken && (<img src={Chevron} onClick={() => setToken(Number(token) - 1)} alt="previous token" className="arrow previousToken" />)}
+        <input type="number" value={token} step="1" onChange={(v) => setToken(v.target.value)} max={endToken} min={startToken} />
+        {token < endToken && (<img src={Chevron} onClick={() => setToken(Number(token) + 1)} alt="next token" className="arrow nextToken" />)}
+      </div>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
